ceddebe86c3c559e1ec5929d06e953dfcd77a985
aclandmark <mark.osborne15@btinternet.com>
2026-06-29 17:57:53 +0100

    Create 328_Hex_verification.h

---
+++ Bootloader_for_328/328_Hex_verification/328_Hex_verification/328_Hex_verification.h
@@ -0,0 +1,13 @@
+
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+#include "../../328_Bootloader_resources\Hex_verification.h"
+#include "../../328_Bootloader_resources\Basic_IO_subroutines.c"
+#include "../../328_Bootloader_resources\Hex_verification_SW_subs.c"
