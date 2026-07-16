086714e0d9ff70d2ec40cde0dafdc5b1db4e6a5d
aclandmark <mark.osborne15@btinternet.com>
2026-07-16 17:44:35 +0100

    Update 1_first_project.ino

--- 2_Short_display_projects/1_first_project/1_first_project.ino
+++ 2_Short_display_projects/1_first_project/1_first_project.ino
@@ -22,7 +22,7 @@ setup_HW;
 
   Port_1 = 1;
   Port_2 = 1;
-for(int m = 0; m <= 15; m++){
+for(int m = 0; m <= 7; m++){
 display_binary (Port_1, Port_2, 5);
 Port_1 = Port_1 << 1;
 Port_2 = Port_2 << 1; 
@@ -35,9 +35,9 @@ int main (void)   //Example 3
   setup_HW;
   wdt_enable(WDTO_30MS);
   Port_1 = 1;
-  for (int m = 0; m <= 15; m++)
+  for (int m = 0; m <= 7; m++)
   {  display_binary (Port_1 << m, Port_1 << m, 5);}
-for (int m = 14; m; m--)
+for (int m = 7; m; m--)
 {  display_binary
 (Port_1 << m, Port_1 << m, 5);}
   while(1); }
@@ -48,9 +48,9 @@ int main (void)    //Example 5
   { unsigned int PORT_1, PORT_2;
   setup_HW;
   while (1)
-  { PORT_1 = 0b0000000000000001; 
-    PORT_2 = 0b1000000000000000;  
-    for (int m = 0; m <= 15; m++)
+  { PORT_1 = 0b00000001; 
+    PORT_2 = 0b10000000;  
+    for (int m = 0; m <= 7; m++)
     { display_binary(PORT_1, PORT_2,2);
       PORT_1 = PORT_1 << 1;
       PORT_2 = PORT_2 >> 1;
@@ -63,8 +63,8 @@ int main (void)    //Example 5
   setup_HW;
   while (1)
   { PORT_1 = 1;
-    PORT_2 = 0x80000000;
-    for (int m = 0; m <= 31; m++)
+    PORT_2 = 0x8000;
+    for (int m = 0; m <= 15; m++)
     {  display_binary(PORT_1, PORT_2, 4);
       PORT_1 = PORT_1 << 1;
       PORT_2 = PORT_2 >> 1;
@@ -74,7 +74,8 @@ int main (void)    //Example 5
 
 ************************************************************
 int main (void)             //Example 8
-  { char digit[] =  "01234567777654321000";
+  { 
+    char digit[] =  "012333321000";
         char letter;
     char counter=0;
     char direction = 0;
@@ -83,19 +84,19 @@ int main (void)             //Example 8
     setup_HW;
   for(int m = 0; m <=7; m++)segments[m] = 0;
   while(1){
-if (counter < 8)letter = 'a';
-if (counter == 8) letter = 'f';
-if (counter == 9) letter = 'e';
-if ((counter >= 10) && (counter <= 17)) letter = 'd';
-if (counter == 18) letter = 'c';
-if (counter == 19) letter = 'b';
-if (counter == 20) letter = 'a';
+if (counter < 4)letter = 'a';
+if (counter == 4) letter = 'f';
+if (counter == 5) letter = 'e';
+if ((counter >= 6) && (counter <= 9)) letter = 'd';
+if (counter == 10) letter = 'c';
+if (counter == 11) letter = 'b';
+if (counter == 12) letter = 'a';
 
  if(!(direction)) segments[digit[counter]-'0'] |= (1 << (letter - 'a')); 
  else segments[digit[counter]-'0'] &= (~(1 << (letter - 'a')));  
   display_pattern(segments, 50);
 counter  += 1;
-if (counter == 20){direction ^= 1; counter = 0;} 
+if (counter == 12){direction ^= 1; counter = 0;} 
 }SW_reset;}
 
 
@@ -112,9 +113,9 @@ if (counter == 20){direction ^= 1; counter = 0;}
   random_num = PRN_16bit_GEN (0, &PRN_counter);
   while (1)
   { for (int m = 0; m < random_num % 3; m++)
-    { if (!(PORT_1 = ((PORT_1 * 2) % 0x10000)))
+    { if (!(PORT_1 = ((PORT_1 * 2) % 0x100)))
         PORT_1 = 1;}
-    if (!(PORT_2 = ((PORT_2 * 2) % 0x10000)))
+    if (!(PORT_2 = ((PORT_2 * 2) % 0x100)))
   PORT_2 = 1;
     
     display_binary (PORT_1, PORT_2, 5);
@@ -128,7 +129,7 @@ if (counter == 20){direction ^= 1; counter = 0;}
 int main (void)     //Example 13  Random display
   {int T1_period = 15;
   char direction;
-  char duration = 50;
+  char duration = 75;
   char seg_counter;
   char segments[8];
   
@@ -154,10 +155,11 @@ int main (void)     //Example 13  Random display
   setup_HW;
   wdt_enable(WDTO_30MS);
   PRN_counter = 0;
-  PRN = PRN_16bit_GEN (0, &PRN_counter);
+  PRN = (PRN_16bit_GEN (0, &PRN_counter));
   while (1)
-  { PRN = PRN_16bit_GEN (PRN, &PRN_counter);
+  { PRN = (PRN_16bit_GEN (PRN, &PRN_counter));
     display_binary (PRN, (PRN << ((PRN % 2) + 1)), 5);}}
+
 
 
   
@@ -176,7 +178,7 @@ duration = waitforkeypress_Basic() - '0';
 
 Port_1 = 1;
 while(1){
-Port_2 = 0x8000 - Port_1;
+Port_2 = 0x80 - Port_1;
 display_binary(Port_1, Port_2, duration);
 if(char_present){if (!(Inc))Inc = 1;
 else Inc = 0;
