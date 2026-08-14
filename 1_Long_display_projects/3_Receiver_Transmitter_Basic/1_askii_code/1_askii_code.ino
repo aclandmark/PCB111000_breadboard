c13247253653049b0e8e0d224f3f7e988c720b16
aclandmark <mark.osborne15@btinternet.com>
2026-08-14 21:57:28 +0100

    Update 1_askii_code.ino

--- 1_Long_display_projects/3_Receiver_Transmitter_Basic/1_askii_code/1_askii_code.ino
+++ 1_Long_display_projects/3_Receiver_Transmitter_Basic/1_askii_code/1_askii_code.ino
@@ -16,7 +16,41 @@
 
 //Ten simple examples to experiment with.
 
+int main (void)                //Example 6
+  { setup_HW;         //Exploring program memory
+  char *mem_add;
+ wdt_enable(WDTO_120MS);
 
+   _delay_ms(1);
+  
+  String_to_PC_Local("\0Null string\r\n");
+  String_to_PC_Local("Defining and using text strings\r\n\r\n");
+  
+  const char *message_1 = "Hello world\r\n";
+  const char *message_2 = "Sending text to a PC\r\n";
+  const char message_3[] = "Writing C programs and\r\n";
+  const char message_4[] = "Uploading them to a device\r\n\r\n";
+  String_to_PC_Local(message_1);
+  String_to_PC_Local(message_2);
+  String_to_PC_Local(message_3);
+  String_to_PC_Local(message_4);
+  while(switch_3_up)wdr();
+
+ String_to_PC_Local("Print out of program memory\r\n\r\n");
+
+mem_add = (char*)256;
+
+for(int m = 0; m <=200; m++){ 
+  if ((*(mem_add + m))== '\0')Hex_and_Int_to_PC_Basic (10,256+m);
+ else Char_to_PC_Basic(*(mem_add + m));}
+
+  newline_Basic();newline_Basic();
+
+  //_delay_ms(1);
+  
+  while(switch_3_down)wdr();
+   
+  return 1;}
 
 
 
