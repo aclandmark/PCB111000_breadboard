dfeb39073348a210264b22c0da4bf116d052a6a9
aclandmark <mark.osborne15@btinternet.com>
2026-09-12 17:23:04 +0100

    Update UNO_programs_328.ino

--- Bootloader_for_328/UNO_programs_328/UNO_programs_328.ino
+++ Bootloader_for_328/UNO_programs_328/UNO_programs_328.ino
@@ -70,6 +70,11 @@ Read_write_mem('I', EE_size - 5, \
 (Atmel_config(signature_bit_3_h, signature_bit_3_l)));       
 
 Read_write_mem('I', 0x3F4, 0xF3);                                          //Initialise EEP locations for PRN use
+Read_write_mem('I', 0x3FC, 0xF3);                                          //Initialise EEP locations for PRN use
+Timer_T0_10mS_delay_x_m(1);
+Read_write_mem('I', 0x3EF, 0xFF);
+
+
 
 sendString("Set BR to 57600 then press AK\r\n");
 USART_init(0,16);
