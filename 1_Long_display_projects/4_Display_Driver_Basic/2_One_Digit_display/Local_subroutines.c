2d9195a0803f9c38b3802409f93c12f7d3a3b6d7
aclandmark <mark.osborne15@btinternet.com>
2026-09-16 15:32:08 +0100

    Update Local_subroutines.c

--- 1_Long_display_projects/4_Display_Driver_Basic/2_One_Digit_display/Local_subroutines.c
+++ 1_Long_display_projects/4_Display_Driver_Basic/2_One_Digit_display/Local_subroutines.c
@@ -2,10 +2,10 @@
 
 
 
-void Char_to_PC_Basic(char);
-void Int_to_PC_Basic(long);
-void String_to_PC_Basic(char*);
-void newline_Basic(void);
+void Char_to_PC_B(char);
+void Int_to_PC_B(long);
+void String_to_PC_B(char*);
+void newline_B(void);
 
 
 #define zero "abcdef"                   //chars a,b,c,d,e and f are stored in an array named "zero"
@@ -22,50 +22,50 @@ void newline_Basic(void);
 
 
 #define print_memory_contents \
-String_to_PC_Basic(message_1);\
-while(1){if (!(*(zero + letter_counter))){Char_to_PC_Basic('0');string_counter += 1;\
+String_to_PC_B(message_1);\
+while(1){if (!(*(zero + letter_counter))){Char_to_PC_B('0');string_counter += 1;\
 if(string_counter == 10)break;\
-}else Char_to_PC_Basic(*(zero + letter_counter));\
+}else Char_to_PC_B(*(zero + letter_counter));\
 letter_counter += 1;}\
 \
-String_to_PC_Basic(message_2);\
+String_to_PC_B(message_2);\
 for(int m = 0; m < 10; m++)String_memory_dump(digit++);\
 \
-Int_to_PC_Basic((int)message_1);\
-newline_Basic();\
-Int_to_PC_Basic((int)message_2);\
-newline_Basic();
+Int_to_PC_B((int)message_1);\
+newline_B();\
+Int_to_PC_B((int)message_2);\
+newline_B();
 
 
 
 
 
 void String_memory_dump(char num){
-Char_to_PC_Basic(num); Char_to_PC_Basic('\t');
+Char_to_PC_B(num); Char_to_PC_B('\t');
 switch(num){
-case '0': Int_to_PC_Basic((int)zero);break;
-case '1': Int_to_PC_Basic((int)one);break;
-case '2': Int_to_PC_Basic((int)two);break;
-case '3': Int_to_PC_Basic((int)three);break;
-case '4': Int_to_PC_Basic((int)four);break;
-case '5': Int_to_PC_Basic((int)five);break;
-case '6': Int_to_PC_Basic((int)six);break;
-case '7': Int_to_PC_Basic((int)seven);break;
-case '8': Int_to_PC_Basic((int)eight);break;
-case '9': Int_to_PC_Basic((int)nine);break;}
-Char_to_PC_Basic('\t');
+case '0': Int_to_PC_B((int)zero);break;
+case '1': Int_to_PC_B((int)one);break;
+case '2': Int_to_PC_B((int)two);break;
+case '3': Int_to_PC_B((int)three);break;
+case '4': Int_to_PC_B((int)four);break;
+case '5': Int_to_PC_B((int)five);break;
+case '6': Int_to_PC_B((int)six);break;
+case '7': Int_to_PC_B((int)seven);break;
+case '8': Int_to_PC_B((int)eight);break;
+case '9': Int_to_PC_B((int)nine);break;}
+Char_to_PC_B('\t');
 switch(num){
-case '0': String_to_PC_Basic(zero);break;
-case '1': String_to_PC_Basic(one);break;
-case '2': String_to_PC_Basic(two);break;
-case '3': String_to_PC_Basic(three);break;
-case '4': String_to_PC_Basic(four);break;
-case '5': String_to_PC_Basic(five);break;
-case '6': String_to_PC_Basic(six);break;
-case '7': String_to_PC_Basic(seven);break;
-case '8': String_to_PC_Basic(eight);break;
-case '9': String_to_PC_Basic(nine);break;}
-newline_Basic();}
+case '0': String_to_PC_B(zero);break;
+case '1': String_to_PC_B(one);break;
+case '2': String_to_PC_B(two);break;
+case '3': String_to_PC_B(three);break;
+case '4': String_to_PC_B(four);break;
+case '5': String_to_PC_B(five);break;
+case '6': String_to_PC_B(six);break;
+case '7': String_to_PC_B(seven);break;
+case '8': String_to_PC_B(eight);break;
+case '9': String_to_PC_B(nine);break;}
+newline_B();}
 
 
 
