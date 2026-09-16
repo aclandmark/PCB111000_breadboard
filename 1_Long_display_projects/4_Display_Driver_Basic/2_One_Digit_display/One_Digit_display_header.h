82022252a57733012c596cfe6a268d81b236b37c
aclandmark <mark.osborne15@btinternet.com>
2026-09-16 15:32:40 +0100

    Update One_Digit_display_header.h

--- 1_Long_display_projects/4_Display_Driver_Basic/2_One_Digit_display/One_Digit_display_header.h
+++ 1_Long_display_projects/4_Display_Driver_Basic/2_One_Digit_display/One_Digit_display_header.h
@@ -2,16 +2,20 @@
 
 #include <avr/wdt.h>
 
-char User_response;
 char watch_dog_reset;
-//char power_on_reset;
+char power_on_reset;
+char User_response;
+char r_prompt;
+
 unsigned int PRN;
 unsigned char PRN_counter;
 
 #define T0_delay_10ms   5,178
 
+void Char_to_PC_Local(char);
 
-/***************************************************************************/
+
+
 #define setup_HW \
 setup_watchdog;\
 ADMUX |= (1 << REFS0);\
@@ -20,12 +24,13 @@ Set_display_drivers;\
 Clear_segments;\
 Clear_digits;\
 set_up_switched_inputs;\
-setup_PC_comms_Basic(0,16);\
-Timer_T0_10mS_delay_x_m(5);
+setup_PC_comms_B(0,16);\
+Timer_T0_10mS_delay_x_m(5);\
+Check_for_r_prompt();\
+Check_for_POR;
 
 
 
-/***************************************************************************/
 #define setup_watchdog \
 if (MCUSR & (1 << WDRF))watch_dog_reset = 1;\
 wdr();\
@@ -38,16 +43,51 @@ WDTCSR = 0;
 #define SW_reset {wdt_enable(WDTO_30MS);while(1);}
 
 
-/*#define Check_for_POR \
-if(MCUSR & (1 << PORF)){power_on_reset = 1;\
-MCUSR &= ~(1<<PORF);}*/
+
+/***********************************************************************************************/
+void Check_for_r_prompt(void){
+  if (!(eeprom_read_byte((uint8_t*)0x3EF) & 0x04))
+  r_prompt = 1;
+  else r_prompt = 0;}
 
 
-/*********************************************************************************/
+
+/***********************************************************************************************/
+#define Check_for_POR \
+if(MCUSR & (1 << PORF))\
+{MCUSR &= ~(1<<PORF);\
+User_prompt_B;\
+r_prompt = 1;}
+
+
+
+
+/***********************************************************************************************/
+#define just_programmed     !(eeprom_read_byte((uint8_t*)0x3EF) & 0x02)
+#define repeat_program      eeprom_write_byte((uint8_t*)0x3EF, ~0x02)
+
+
+
+/***********************************************************************************************/
+#define clear_resets \
+eeprom_write_byte((uint8_t*)0x3EF, 0xFF);\
+watch_dog_reset = 0;
+
+
+/***********************************************************************************************/
 #define Set_display_drivers \
 DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5);\
 DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
 DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
+
+
+
+/***********************************************************************************************/
+#define Clear_segments    a_off;b_off;c_off;d_off;e_off;f_off;g_off;dp_off;
+
+#define Clear_digits \
+digit_1_RH_off;digit_2_RH_off;digit_3_RH_off;digit_4_RH_off;\
+digit_1_LH_off;digit_2_LH_off;digit_3_LH_off;digit_4_LH_off;
 
 
 
@@ -60,11 +100,31 @@ PORTC |= ((1 << PC5) | (1 << PC4));
 
 
 /***********************************************************************************************/
-#define User_prompt_Basic \
+#define switch_1_down ((PINC & 0x20)^0x20)
+#define switch_1_up   (PINC & 0x20)
+#define switch_2_down ((PINC & 0x10)^0x10)
+#define switch_2_up   (PINC & 0x10)
+
+
+
+/***********************************************************************************************/
+#define set_IO_WPU \
+MCUCR &= (~(1 << PUD));\
+DDRB = 0;\
+DDRC = 0;\
+DDRD = 0;\
+PORTB = 0xFF;\
+PORTC = 0xFF;\
+PORTD = 0xFF;
+
+
+
+/***********************************************************************************************/
+#define User_prompt_B \
 while(1){\
-do{String_to_PC_Basic("R?    ");}  while((isCharavailable_Basic (50) == 0));\
-User_response = Char_from_PC_Basic();\
-if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("\r\n");
+do{String_to_PC_B("R?    ");}  while((isCharavailable_B (50) == 0));\
+User_response = Char_from_PC_B();\
+if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_B("\r\n");
 
 
 
@@ -73,12 +133,6 @@ if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("
 if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
 &&  (eeprom_read_byte((uint8_t*)0x3FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x3FE)\
 == eeprom_read_byte((uint8_t*)0x3FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}
-
-
-
-/********************************************************/
-#define first_run_after_programming   !(eeprom_read_byte((uint8_t*)0x3FA))
-#define clear_programmer              eeprom_write_byte((uint8_t*)0x3FA, 0xFF);
 
 
 
