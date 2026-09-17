c70d80066a6417339277772d62049ae8a8e5190e
aclandmark <mark.osborne15@btinternet.com>
2026-09-17 18:08:49 +0100

    Update FPN_from_KBD_header.h

--- 1_Long_display_projects/9_Real_numbers/2_FPN_from_KBD/FPN_from_KBD_header.h
+++ 1_Long_display_projects/9_Real_numbers/2_FPN_from_KBD/FPN_from_KBD_header.h
@@ -1,33 +1,39 @@
-
-
 //#define F_CPU 8000000
 
 #include <avr/wdt.h>
 
-char User_response;
 char watch_dog_reset;
 char power_on_reset;
+char User_response;
+char r_prompt;
+
 unsigned int PRN;
 unsigned char PRN_counter;
 
 #define T0_delay_10ms   5,178
 
+void Char_to_PC_Local(char);
 
-/***************************************************************************/
+
+
 #define setup_HW \
 setup_watchdog;\
-Check_for_POR;\
 ADMUX |= (1 << REFS0);\
 OSC_CAL;\
-Set_display_drivers;\
+\
 set_up_switched_inputs;\
 Serial.begin(115200);\
 while (!Serial);\
-Timer_T0_10mS_delay_x_m(5);
+Timer_T0_10mS_delay_x_m(5);\
+Check_for_r_prompt();\
+Check_for_POR;
 
+/*
+ Set_display_drivers;\
+Clear_segments;\
+Clear_digits;
+ */
 
-
-/***************************************************************************/
 #define setup_watchdog \
 if (MCUSR & (1 << WDRF))watch_dog_reset = 1;\
 wdr();\
@@ -40,16 +46,51 @@ WDTCSR = 0;
 #define SW_reset {wdt_enable(WDTO_30MS);while(1);}
 
 
+
+/***********************************************************************************************/
+void Check_for_r_prompt(void){
+  if (!(eeprom_read_byte((uint8_t*)0x3EF) & 0x04))
+  r_prompt = 1;
+  else r_prompt = 0;}
+
+
+
+/***********************************************************************************************/
 #define Check_for_POR \
-if(MCUSR & (1 << PORF)){power_on_reset = 1;\
-MCUSR &= ~(1<<PORF);}
+if(MCUSR & (1 << PORF))\
+{MCUSR &= ~(1<<PORF);\
+User_prompt_A;\
+r_prompt = 1;}
 
 
-/*********************************************************************************/
+
+
+/***********************************************************************************************/
+#define just_programmed     !(eeprom_read_byte((uint8_t*)0x3EF) & 0x02)
+#define repeat_program      eeprom_write_byte((uint8_t*)0x3EF, ~0x02)
+
+
+
+/***********************************************************************************************/
+#define clear_resets \
+eeprom_write_byte((uint8_t*)0x3EF, 0xFF);\
+watch_dog_reset = 0;
+
+
+/***********************************************************************************************/
 #define Set_display_drivers \
 DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5);\
 DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
 DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
+
+
+
+/***********************************************************************************************/
+#define Clear_segments    a_off;b_off;c_off;d_off;e_off;f_off;g_off;dp_off;
+
+#define Clear_digits \
+digit_1_RH_off;digit_2_RH_off;digit_3_RH_off;digit_4_RH_off;\
+digit_1_LH_off;digit_2_LH_off;digit_3_LH_off;digit_4_LH_off;
 
 
 
@@ -61,11 +102,23 @@ PORTC |= ((1 << PC5) | (1 << PC4));
 
 
 
-/**********************************************************************************************/
-#define switch_3_down ((PINC & 0x20)^0x20)
-#define switch_3_up   (PINC & 0x20)
+/***********************************************************************************************/
+#define switch_1_down ((PINC & 0x20)^0x20)
+#define switch_1_up   (PINC & 0x20)
 #define switch_2_down ((PINC & 0x10)^0x10)
 #define switch_2_up   (PINC & 0x10)
+
+
+
+/***********************************************************************************************/
+#define set_IO_WPU \
+MCUCR &= (~(1 << PUD));\
+DDRB = 0;\
+DDRC = 0;\
+DDRD = 0;\
+PORTB = 0xFF;\
+PORTC = 0xFF;\
+PORTD = 0xFF;
 
 
 
@@ -86,19 +139,9 @@ if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
 
 
 
-/********************************************************/
-#define first_run                    !(eeprom_read_byte((uint8_t*)0x3FA))
-#define clear_first_run              eeprom_write_byte((uint8_t*)0x3FA, 0xFF);
-#define record_POR                    eeprom_write_byte((uint8_t*)0x3FA, 0);
-#define clear_POR                     eeprom_write_byte((uint8_t*)0x3FA, 0xFF);
-
-
 /*****************************************************************************/
 #include "328_Resources/Subroutines/HW_timers.c"
 #include "328_Resources/PC_comms/Arduino.c"
-//#include "328_Resources/PC_comms/Arduino_Rx_Tx_Extra.c"
-//#include "328_Resources/Subroutines/Random_and_prime_nos.c"
-//#include "328_Resources/PC_comms/Real_num_resources.c"
 
 
 
